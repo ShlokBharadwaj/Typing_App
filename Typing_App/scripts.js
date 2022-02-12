e8bfdf55d29e4083a5c3b175f8be14ccfb26ba21
Shlok <shlokbharadwaj7@gmail.com>
2022-02-12 22:23:30 +0530

    Added working

--- Typing_App/scripts.js
+++ Typing_App/scripts.js
@@ -1,25 +1,63 @@
-const RANDOM_WORD_API_URL = "https://random-word-api.herokuapp.com/word?number=10&swear=0"
+const RANDOM_WORD_API_URL = 'http://api.quotable.io/random'
 const wordsDisplayElements = document.getElementById('wordsDisplay')
 const wordsInputElements = document.getElementById('wordsInput')
+const timerElement = document.getElementById('timer')
 
+wordsInputElements.addEventListener('input', () => {
+    const arrayQuote = wordsDisplayElements.querySelectorAll('span')
+    const arrayValue = wordsInputElements.value.split('')
+
+  let correct = true
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index]
+    if (character == null) {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.remove('incorrect')
+      correct = false
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add('correct')
+      characterSpan.classList.remove('incorrect')
+    } else {
+      characterSpan.classList.remove('correct')
+      characterSpan.classList.add('incorrect')
+      correct = false
+    }
+})
+
+if (correct) generateNewWords()
+})
 
 function getRandomWords(){
     return fetch(RANDOM_WORD_API_URL)
     .then(response => response.json())
+    .then(data => data.content)
 }
 
-
 async function generateNewWords(){
-    const words = await getRandomWords()
-    //console.log((words.slice(0,10)).join(' '));
-    wordsDisplayElements.innerText = words.slice(0,10).join(' ');
-    wordsInputElements.value = null;
-    words.split('').forEach(character => {
-        const characterSpan = document.createElement('span')
-        characterSpan.innerText = character
-        wordsDisplayElements.appendChild(characterSpan)
-    });
-    
+         const words = await getRandomWords()
+
+        wordsDisplayElements.innerHTML =  ''
+
+        words.split('').forEach(character => {
+            const characterSpan = document.createElement('span')
+            characterSpan.innerText = character
+            wordsDisplayElements.appendChild(characterSpan)
+        })
+        wordsInputElements.value=null
+        startTimer()
 } 
+
+let startTime
+function startTimer() {
+  timerElement.innerText = 0
+  startTime = new Date()
+  setInterval(() => {
+    timer.innerText = getTimerTime()
+  }, 1000)
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000)
+}
 
 generateNewWords();
